5bde67277116e6cca71b48ac034ee473c096d44a
Spencer Ng <sng1488@gmail.com>
2021-06-01 16:59:24 -0500

    Complete card flip animation; rough interior layout

--- Assets/Scripts/PlayingCardGenerator.cs
+++ Assets/Scripts/PlayingCardGenerator.cs
@@ -6,19 +6,40 @@ using UnityEditor;
 
 public class PlayingCardGenerator : MonoBehaviour
 {
+
+    ArrayList allCards;
+    GameObject guidingBall;
+    bool isPlaying;
+    Animator animatorComponent;
+    private bool faceUp;
+    private bool isRed;
+
+    void Awake() {
+        guidingBall = this.gameObject.transform.GetChild(1).gameObject;
+        animatorComponent = GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        faceUp = true;
+        isRed = false;
+        isPlaying = false;
         GameObject childCard = this.gameObject.transform.GetChild(0).gameObject;
+        
+
+        allCards = new ArrayList();
+        allCards.Add(childCard);
 
         float x, y, z;
         x = childCard.transform.position.x;
         y = childCard.transform.position.y;
         z = childCard.transform.position.z;
 
-        for (int i = 0; i <= 51; i++) {
+        for (int i = 1; i <= 51; i++) {
             GameObject newCard = Instantiate(childCard, gameObject.transform);
-            newCard.transform.position = transform.position + new Vector3(0.03f * i, 0.03f * i, 0);
+            allCards.Add(newCard);
+            newCard.transform.position = transform.position + new Vector3(0.03f * i, 0.0001f * i, 0);
 
             string value = (i % 13 + 1).ToString();
 
@@ -46,7 +67,7 @@ public class PlayingCardGenerator : MonoBehaviour
             Debug.Log(value + suit);
 
             Texture cardFace = AssetDatabase.LoadAssetAtPath<Texture>("Assets/Textures/cards_texture/" + value + suit + ".png");
-            newCard.GetComponent<Renderer>().materials[1].SetTexture("_MainTex", cardFace);
+            newCard.GetComponent<Renderer>().materials[2].SetTexture("_MainTex", cardFace);
         }
     }
 
@@ -54,5 +75,57 @@ public class PlayingCardGenerator : MonoBehaviour
     void Update()
     {
         
+        if (!animatorComponent.enabled && Input.GetKeyDown(KeyCode.E)) {
+            Debug.Log("E Pressed");
+            isPlaying = true;
+            animatorComponent.enabled = true;
+            
+            if (faceUp) {
+                animatorComponent.SetFloat("Direction", 1.0f);
+            } else {
+                animatorComponent.SetFloat("Direction", -1.0f);
+            }
+            animatorComponent.Play("CardLeftToRight");
+        }
+    }
+
+    public void StartPoint() {
+        if (animatorComponent.GetFloat("Direction") == -1.0f) {
+            Disable();
+        } 
+    }
+
+    public void EndPoint() {
+        if (animatorComponent.GetFloat("Direction") == 1.0f) {
+            Disable();
+        } 
+    }
+
+    public void Disable() {
+        isPlaying = false;
+        animatorComponent.enabled = false;
+        
+        // Transform card back color
+        if (!faceUp) {
+            isRed = !isRed;
+            Texture cardBack;
+            if (isRed) {
+                cardBack = AssetDatabase.LoadAssetAtPath<Texture>("Assets/Textures/red_rider_back.jpg");
+            } else {
+                cardBack = AssetDatabase.LoadAssetAtPath<Texture>("Assets/Textures/blue_rider_back.jpg");
+            }
+
+            foreach (GameObject card in allCards) {
+                card.GetComponent<Renderer>().materials[1].SetTexture("_MainTex", cardBack);
+            }
+
+        }
+        Debug.Log(faceUp);
+        faceUp = !faceUp;
+        Debug.Log(faceUp);
+    }
+
+    public void Enable() {
+        isPlaying = true;
     }
 }
